695785d3469ec86076e3d2379d4112f86d2fb92e
Paulo Melo <peimelo@gmail.com>
2021-03-12 15:51:58 -0300

    Adds configuration on devise_token_auth

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -57,4 +57,18 @@ DeviseTokenAuth.setup do |config|
   # devise confirmable module. If you want to use devise confirmable module and
   # send email, set it to true. (This is a setting for compatibility)
   config.send_confirmation_email = true
+
+  # By default this value is expected to be sent by the client so that the API
+  # knows where to redirect users after successful email confirmation. If this
+  # param is set, the API will redirect to this value when no value is provided by the client.
+  config.default_confirm_success_url = 'http://localhost:4200/login'
+
+  # By default this value is expected to be sent by the client so that the API
+  # knows where to redirect users after successful password resets. If this
+  # param is set, the API will redirect to this value when no value is provided by the client.
+  config.default_password_reset_url = 'http://localhost:4200/reset-password'
+
+  # By default, old tokens are not invalidated when password is changed. Enable
+  # this option if you want to make passwords updates to logout other devices.
+  config.remove_tokens_after_password_reset = true
 end
