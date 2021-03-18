e0fcc859aee5b57c57b96296482ef3b40b92e427
Paulo Melo <peimelo@gmail.com>
2021-03-18 19:47:17 -0300

    Adds fields that can be changed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name nickname])
+  end
 end
