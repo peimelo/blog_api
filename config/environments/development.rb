3fb9c0e38cd51734f6ba473f93118d550baa5e86
Paulo Melo <peimelo@gmail.com>
2021-04-19 05:58:44 -0300

    Adds default url option for dev and test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,7 +66,4 @@ Rails.application.configure do
   # Letter opener
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
-
-  # Devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
